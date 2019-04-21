107c1db72af15ef9b0736fd829c97080ecd79ab5
Felix Benetzeder <felix.benetzeder@blackrock.com>
2019-04-21 20:40:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,8 +4,10 @@ def roll_call_dwarves(dwarfs)
   end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.map do |item|
+    puts item
+  end
 end
 
 def long_planeteer_calls# code an argument here
