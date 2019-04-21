24690db3c9b07faaed0459066bf01544a12a9395
Felix Benetzeder <felix.benetzeder@blackrock.com>
2019-04-21 20:48:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,6 +17,7 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do |food|
+    ch
 end
